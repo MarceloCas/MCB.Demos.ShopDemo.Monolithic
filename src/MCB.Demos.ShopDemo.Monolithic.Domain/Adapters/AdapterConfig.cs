35903f428fc7ed0619ff9122aad80a86c2671746
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-12-04 21:36:05 -0300

    feat: add adapter for ValidationMessage and Notification

--- src/MCB.Demos.ShopDemo.Monolithic.Domain/Adapters/AdapterConfig.cs
+++ src/MCB.Demos.ShopDemo.Monolithic.Domain/Adapters/AdapterConfig.cs
@@ -1,4 +1,7 @@
 ﻿using Mapster;
+using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Notifications.Models;
+using MCB.Core.Infra.CrossCutting.DesignPatterns.Abstractions.Notifications.Models.Enums;
+using MCB.Core.Infra.CrossCutting.DesignPatterns.Validator.Abstractions.Models;
 using MCB.Demos.ShopDemo.Monolithic.Domain.Entities.Customers.Inputs;
 using MCB.Demos.ShopDemo.Monolithic.Domain.Services.Customers.Inputs;
 
@@ -10,5 +13,14 @@ public class AdapterConfig
     public static void Configure(TypeAdapterConfig typeAdapterConfig)
     {
         typeAdapterConfig.ForType<RegisterNewCustomerServiceInput, RegisterNewCustomerInput>();
+        typeAdapterConfig.ForType<ValidationMessage, Notification>()
+            .MapWith(
+                converterFactory: src => new Notification(
+                    (NotificationType)(int)src.ValidationMessageType,
+                    src.Code,
+                    src.Description
+                ),
+                applySettings: true
+            );
     }
 }
