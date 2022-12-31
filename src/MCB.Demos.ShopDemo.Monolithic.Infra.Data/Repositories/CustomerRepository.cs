b2ae36eeb86ba624a774989d2a1b0a3a4e18fa4a
Marcelo Castelo Branco <marcelo.castelo@outlook.com>
2022-12-31 19:16:26 -0300

    code smell

--- src/MCB.Demos.ShopDemo.Monolithic.Infra.Data/Repositories/CustomerRepository.cs
+++ src/MCB.Demos.ShopDemo.Monolithic.Infra.Data/Repositories/CustomerRepository.cs
@@ -83,11 +83,13 @@ public class CustomerRepository
 
     public async Task<Customer?> GetByEmailAsync(Guid tenantId, string email, CancellationToken cancellationToken)
     {
-        var customerDataModel = (await _customerDataModelRepository.GetAsync(
-            q => q.TenantId == tenantId 
-            && q.Email == email, 
-            cancellationToken
-            )).FirstOrDefault();
+        var customerDataModel = (
+            await _customerDataModelRepository.GetAsync(q => 
+                q.TenantId == tenantId 
+                && q.Email == email, 
+                cancellationToken
+            )
+        ).FirstOrDefault();
 
         return customerDataModel is null
             ? null
